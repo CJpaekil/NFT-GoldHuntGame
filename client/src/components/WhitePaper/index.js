539ca0ddd12a8f4d9c2e291af5b306c808bde93f
Senior_DEV <86672166+CJpaekil@users.noreply.github.com>
2021-12-14 18:01:54 +0900

    all updated

--- client/src/components/WhitePaper/index.js
+++ client/src/components/WhitePaper/index.js
@@ -115,7 +115,7 @@ const Index = ({
                     <button class="header-button"  onClick={() => setShow(true)}>WHITEPAPER</button>
                 </div>
             </div>
-            <div>zxc</div>
+            <div>zxcdfgd</div>
             <div>
             <Minting/>
             </div>   
